2d97289fcb963653b08cd23b4b0db53bf10b37dc
Marta Milewczyk <96419859+MartaMilewczyk@users.noreply.github.com>
2023-02-14 15:20:14 +0100

    NASA API test update

--- script.js
+++ script.js
@@ -18,8 +18,25 @@ $('#image').on('click', function (event) {
         const explanation = $('<p>').append(response.explanation)
         
         $('#display-photo').append(image, date, explanation)
-
     })
+})
+
+$('#image-gallery').on('click', function (event) {
+    event.preventDefault();
+
+    const nasa_id = "PIA18033"
+    let queryURL = `https://images-api.nasa.gov/asset/${nasa_id}`
 
 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+
+        const image = $('<img>').attr('src', response.collection.items[3].href);
+        
+        $('#display-photo').append(image)
     })
+})
+
